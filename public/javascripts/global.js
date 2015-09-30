93eb6151ea8f7396d03e236713be63c8dacbb432
Nathan Connor <natyconnor@gmail.com>
2015-09-29 23:50:23 -0700

    Added form for adding users which adds them via AJAX and updates the table

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -9,6 +9,9 @@ $(document).ready(function() {
 
     // Username link click
     $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+
+    // Add User button click
+    $('#btnAddUser').on('click', addUser);
 });
 
 // Functions ======================================
@@ -56,4 +59,59 @@ function showUserInfo(event) {
     $('#userInfoAge').text(thisUserObject.age);
     $('#userInfoGender').text(thisUserObject.gender);
     $('#userInfoLocation').text(thisUserObject.location);
+}
+
+function addUser(event) {
+	event.preventDefault();
+
+	// Super basic validation -- increase errorCount variable if any fields are blank
+	var errorCount = 0;
+	$('#addUser input').each(function(index, val) {
+		if($(this).val() === '') { errorCount++; }
+	});
+
+	// Check and make sure errorCount's still at zero
+	if(errorCount === 0) {
+		// If it is, compile all user info into one object
+		var newUser = {
+			'username': $('#inputUserName').val(),
+            'email': $('#inputUserEmail').val(),
+            'fullname': $('#inputUserFullname').val(),
+            'age': $('#inputUserAge').val(),
+            'location': $('#inputUserLocation').val(),
+            'gender': $('#inputUserGender').val()
+		}
+
+		// Use AJAX to post the object to our adduser service
+		$.ajax({
+			url: 'users/adduser',
+			type: 'POST',
+			dataType: 'JSON',
+			data: newUser
+		})
+		.done(function(response) {
+			// Check for successful (blank) response
+			if(response.msg === '') {
+				// Clear the form inputs
+				$('#addUser fieldset input').val('');
+
+				// Update the table
+				populateTable();
+			} else {
+				// If something goes wrong, alert the error message that our service returned
+				alert('Error: ' + response.msg);
+			}
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+		
+	} else {
+		// If errorCount is more than 0, error out
+		alert('Please fill in all fields');
+		return false;
+	}
 }
